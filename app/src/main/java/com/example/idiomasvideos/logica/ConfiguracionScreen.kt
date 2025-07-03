dceaed48bbd591734c7cccaa67e8ea3f1f3f2a1b
Bgonfer <bgonfer.genius@gmail.com>
2025-07-03 14:16:35 +0200

    Rama 7 primer mensaje

--- app/src/main/java/com/example/idiomasvideos/logica/ConfiguracionScreen.kt
+++ app/src/main/java/com/example/idiomasvideos/logica/ConfiguracionScreen.kt
@@ -1,5 +1,6 @@
 package com.example.idiomasvideos.logica
 
+import android.graphics.Paint
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -10,13 +11,17 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.idiomasvideos.R
 import com.example.idiomasvideos.logica.Helper.UsarBotonSwitch
 //A diferencia de TextoPruebaScreen, aqui no pondre los parametros de las funciones ya que no van a cambiar
@@ -30,20 +35,20 @@ fun ConfiguracionScreen(
     ) {
         fondoConfigurationScreen()
         Column(
-            modifier = Modifier
+            modifier = modifier
                 .fillMaxSize()
                 .then(modifier)
         ) {
             Row(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .fillMaxHeight(0.2f),
+                    .fillMaxHeight(0.1f),
                 horizontalArrangement = Arrangement.Center,
                 verticalAlignment = Alignment.CenterVertically
             ) {
                 tituloConfigurationScreen()
             }
-            Spacer(modifier = Modifier.height(30.dp))
+           Spacer(modifier = Modifier.height(10.dp))
 
             Row {
                 UsarBotonSwitch()
@@ -60,11 +65,15 @@ fun ConfiguracionScreen(
         }
     }
 }
-
 @Composable
 fun tituloConfigurationScreen(){
     Text(
-        text = stringResource(R.string.tituloConfiguracion))
+        text = stringResource(R.string.tituloConfiguracion),
+        textAlign = TextAlign.Center,
+        fontSize = 30.sp,
+        color = Color.Blue
+
+    )
 
 }
 
--- app/src/main/java/com/example/idiomasvideos/pantallas/MostrarConfiguracionScreen.kt
+++ app/src/main/java/com/example/idiomasvideos/pantallas/MostrarConfiguracionScreen.kt
@@ -2,6 +2,7 @@ package com.example.idiomasvideos.pantallas
 
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
@@ -19,7 +20,8 @@ fun MostrarConfiguracionScreen (
         content = { padding ->
             ConfiguracionScreen(
                 modifier = modifier
-                    .fillMaxSize(),)
+                    .fillMaxSize()
+                    .padding(padding),)
             content(padding)
         }
     )
